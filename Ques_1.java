cb60c3644a1001cd3e862fcecdde3516e816ff0c
ashish-kattamuri <69315739+ashish-kattamuri@users.noreply.github.com>
2022-02-02 13:00:13 +0530

    Update Ques_1.java

--- Ques_1.java
+++ Ques_1.java
@@ -1,23 +1,8 @@
 
-class Vampirenumber {
-	Boolean s_Flag = false;
-	//breaking the number into individual number and storing in an array.
-	public int[] breakNumber(int i_Number) {
-		int remainder= 0;
-		int index=0;
-		int length = (int) (Math.log10(i_Number) + 1);
-		int array[] = new int[length];
-		while (i_Number != 0) {
-			remainder = i_Number % 10;
-			array[index] = remainder;
-			i_Number = i_Number / 10;
-			index++;
-		}
-		return array;
-	}
+
 	//converting the array to two equal parts and find the vampire number
 
-	Boolean checkVampire(int[] arr, int i_Number) {
+	Boolean checkVampire(int[] arr, int num) {
 		int firstPart = 0;
 		int secondPart = 0;
 		for (int index = 0; index < (arr.length) / 2; index++) {
@@ -26,25 +11,25 @@ class Vampirenumber {
 		for (int index = (arr.length) / 2; index < arr.length; index++) {
 			secondPart += arr[index] * Math.pow(10, (arr.length - index - 1));
 		}
-		if (firstPart * secondPart == i_Number) {
+		if (firstPart * secondPart == num) {
 			return true;
 		}
 		return false;
 	}
 
 	//finds all the permutations of the given input array and returns to the checkvampire method.
-	void permutaionFinder(int[] arr, int index, int i_Number) {
+	void permutaionFinder(int[] arr, int index, int num) {
 		if (index >= arr.length - 1) {
-			if (this.checkVampire(arr, i_Number)) {
+			if (this.checkVampire(arr, num)) {
 				//flag will set to true if it is a vampire number.
-				s_Flag = true;
+				isFlag = true;
 			}
 		}
 		for (int i = index; i < arr.length; i++) { 
 			int t = arr[index];
 			arr[index] = arr[i];
 			arr[i] = t;
-			permutaionFinder(arr, index + 1, i_Number);
+			permutaionFinder(arr, index + 1, num);
 			t = arr[index];
 			arr[index] = arr[i];
 			arr[i] = t;
@@ -52,20 +37,38 @@ class Vampirenumber {
 	}
 };
 
-public class Ques_1 {
+class Vampirenumber {
+	Boolean isFlag = false;
+	//breaking the number into individual number and storing in an array.
+	public int[] breakNumber(int num) {
+		int remainder= 0;
+		int index=0;
+		int length = (int) (Math.log10(num) + 1);
+		int array[] = new int[length];
+		while (num != 0) {
+			remainder = num % 10;
+			array[index] = remainder;
+			num = num / 10;
+			index++;
+		}
+		return array;
+	}
+
+public class Ques1 {
 
 	public static void main(String[] args) {
-		Vampirenumber vampire = new Vampirenumber();
+		
 		int number = 1001;
-		int counter = 0;
-		while (counter < 100) {
+		int count = 0;
+		Vampirenumber vampire = new Vampirenumber();
+		while (count < 100) {
 			if (((int) (Math.log10(number) + 1))%2 == 0) {
 				int[] array = vampire.breakNumber(number);
 				vampire.permutaionFinder(array, 0, number);
-				if (vampire.s_Flag) {
+				if (vampire.isFlag) {
 					System.out.println(number);
-					counter++;
-					vampire.s_Flag = false;
+					count++;
+					vampire.isFlag = false;
 				}
 
 			}
